0bb7070e23cca30956fe79f2bfc701822c024956
GiovanaJensen <oliveirasgiovana143@gmail.com>
2024-03-20 20:29:31 -0300

    add: header structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:alubank/components/sections/Header.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Header(),);
+  }
+}
